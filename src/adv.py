6473514a908dc06110fbc29cf26a50b92ddcd193
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-02-13 13:53:08 +0000

    Add items to rooms

--- src/adv.py
+++ src/adv.py
@@ -1,20 +1,24 @@
 import os
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     [Item("Torch"),]),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
+    'foyer':    Room("Foyer", 
+                     "Dim light filters in from the south. Dusty passages run north and east.",
+                     [Item("Sword")]),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
 }
 
 
